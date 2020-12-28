60eb99050311a9880b6232b80ba2c4e79ab18092
kenkaneki <lunafreya2398@gmail.com>
2020-12-28 20:29:43 +0700

    add Details component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/Details/Details";
+
+import useStyles from "./styles";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Thu nhập" />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Chi tiêu" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
