c5e13e60d9e1d86df79ddca86f16cc02da78dbd1
kenkaneki <lunafreya2398@gmail.com>
2020-12-29 19:37:23 +0700

    add and use Context API

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   TextField,
   Typography,
@@ -9,11 +9,34 @@ import {
   Select,
   MenuItem,
 } from "@material-ui/core";
+import { v4 as uuidv4 } from "uuid";
+import { ExpenseTrackerContext } from "../../../context/context";
 
 import useStyles from "./styles";
 
+const initialState = {
+  amount: "",
+  category: "",
+  type: "Thu nhập",
+  date: new Date(),
+};
+
 const Form = () => {
   const classes = useStyles();
+  const [formData, setFormData] = useState(initialState);
+  const { addTransaction } = useContext(ExpenseTrackerContext);
+
+  const createTransaction = () => {
+    const transaction = {
+      ...formData,
+      amount: Number(formData.amount),
+      id: uuidv4(),
+    };
+
+    addTransaction(transaction);
+    setFormData(initialState);
+  };
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -24,7 +47,10 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Kiểu</InputLabel>
-          <Select>
+          <Select
+            value={formData.type}
+            onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+          >
             <MenuItem value="Income">Thu nhập</MenuItem>
             <MenuItem value="Expense">Chi tiêu</MenuItem>
           </Select>
@@ -33,23 +59,39 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Loại</InputLabel>
-          <Select>
+          <Select
+            value={formData.category}
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <MenuItem value="business">Kinh doanh</MenuItem>
             <MenuItem value="salary">Lương</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type="number"
+          label="Số lượng"
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+          fullWidth
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth />
+        <TextField
+          type="date"
+          label="Ngày tháng"
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+          fullWidth
+        />
       </Grid>
       <Button
         className={classes.button}
         variant="outlined"
         color="primary"
         fullWidth
+        onClick={createTransaction}
       >
         Tạo
       </Button>
--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   List as MUIList,
   ListItem,
@@ -11,34 +11,12 @@ import {
 } from "@material-ui/core";
 import { Delete, MoneyOff } from "@material-ui/icons";
 
+import { ExpenseTrackerContext } from "../../../context/context";
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
-
-  const transactions = [
-    {
-      id: 1,
-      type: "Thu nhập",
-      category: "Lương",
-      amount: 50,
-      date: "Mon Dec 28",
-    },
-    {
-      id: 2,
-      type: "Chi tiêu",
-      category: "Thú nuôi",
-      amount: 50,
-      date: "Tue Dec 29",
-    },
-    {
-      id: 3,
-      type: "Thu nhập",
-      category: "Kinh doanh",
-      amount: 150,
-      date: "Wed Dec 30",
-    },
-  ];
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
 
   return (
     <MUIList dense={false} className={classes.list}>
@@ -67,7 +45,11 @@ const List = () => {
               secondary={`$${transaction.amount} - ${transaction.date}`}
             />
             <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete" onClick="">
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => deleteTransaction(transaction.id)}
+              >
                 <Delete />
               </IconButton>
             </ListItemSecondaryAction>
